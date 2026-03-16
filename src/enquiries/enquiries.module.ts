b313529410911826375c28d1dfa1924c5e0ed303
CherlyIC <cherlyimfurankunda@gmail.com>
2026-03-16 15:18:05 +0200

    Generated controller and services for all modules

--- src/enquiries/enquiries.module.ts
+++ src/enquiries/enquiries.module.ts
@@ -1,4 +1,9 @@
 import { Module } from '@nestjs/common';
+import { EnquiriesController } from './enquiries.controller';
+import { EnquiriesService } from './enquiries.service';
 
-@Module({})
+@Module({
+  controllers: [EnquiriesController],
+  providers: [EnquiriesService]
+})
 export class EnquiriesModule {}
